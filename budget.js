c34bf3ed146b87cc330c0e9037f4f48bdba7e5fe
josedanielasn <63945372+josedanielasn@users.noreply.github.com>
2020-06-24 19:03:25 +0800

    latest progress

--- budget.js
+++ budget.js
@@ -3,18 +3,84 @@ const select = add.querySelector('.add__type');
 const description = add.querySelector('.add__description');
 const inc_exp = add.querySelector('.add__value');
 const add_btn = add.querySelector('.add__btn');
-// const inc_list = document.querySelector('.income__list');
-// const exp_list = document.querySelector('.expenses__list');
-
+const inc_list = document.querySelector('.income__list');
+const exp_list = document.querySelector('.expenses__list');
+const percentage_dis = document.querySelector('.budget__expenses--percentage');
+let inv_percentage,
+  inc_array = [],
+  exp_array = [],
+  newobj;
+add_btn.addEventListener('click', get_input_btn);
 description.addEventListener('keypress', get_input);
 inc_exp.addEventListener('keypress', get_input);
-add_btn.addEventListener('click', get_input_btn);
+
+let Income_expense = class {
+  constructor(Description, Select, inc_exp) {
+    this.Description = Description;
+    this.Select = Select;
+    this.inc_exp = inc_exp;
+  }
+  where_to_print() {
+    let sign, current_array, list_point, percentage_print;
+    let formated_number = formatNumber(parseFloat(this.inc_exp));
+    total_inc(this.Select, this.inc_exp);
+    if (this.Select === 'inc') {
+      sign = '+';
+      current_array = inc_array;
+      list_point = inc_list;
+      percentage_print = '';
+    }
+    if (this.Select === 'exp') {
+      sign = '-';
+      current_array = exp_array;
+      list_point = exp_list;
+      percentage_print = `<div class="item__percentage">${inv_percentage}</div>`;
+    }
+    const print_list = function (list_point, current_array) {
+      const htmlstring = current_array
+        .map(function (element) {
+          return `<div class="item clearfix" id="expense-0">
+        <div class="item__description">${element.Description}</div>
+        <div class="right clearfix">
+        <div class="item__value">${sign} ${formatNumber(element.inc_exp)}</div>
+        ${percentage_print}
+        <div class="item__delete">
+        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+        </div>
+        </div>
+        </div>`;
+        })
+        .join('');
+      list_point.innerHTML = htmlstring;
+      console.log('PRINT');
+    };
+    current_array.push(this);
+    total_inc(this.Select, this.inc_exp);
+    print_list(list_point, current_array);
+    display_upperScreen(formated_number, this.Select, sign);
+    this.rem_ind(current_array, this, list_point);
+  }
+
+  rem_ind(array, elem, list_point) {
+    list_point.addEventListener('click', function (e) {
+      if (e.target.matches('i')) {
+        let current_node = e.target.parentNode.parentNode.parentNode.parentNode;
+        console.log('joseDaniel');
+        console.log(e.target.parentNode.parentNode.parentNode.parentNode);
+        array.splice(array.indexOf(elem), 1);
+        list_point.removeChild(current_node);
+        total_inc();
+      }
+    });
+  }
+};
 
 function get_input_btn() {
   console.log(select.value);
   console.log(description.value);
   console.log(inc_exp.value);
-  where_to_print();
+  newobj = new Income_expense(description.value, select.value, inc_exp.value);
+  newobj.where_to_print();
 }
 
 function get_input(e) {
@@ -22,43 +88,9 @@ function get_input(e) {
     console.log(select.value);
     console.log(description.value);
     console.log(inc_exp.value);
-    where_to_print();
+    newobj = new Income_expense(description.value, select.value, inc_exp.value);
+    newobj.where_to_print();
   }
-}
-
-function where_to_print() {
-  const select_value = select.value;
-  const description_value = description.value;
-  const Inc_exp_value = inc_exp.value;
-  const inc_exp_value = formatNumber(inc_exp.value);
-  let sign, print_to;
-  if (select_value === 'inc') {
-    sign = '+';
-    print_to = document.querySelector('.income__list');
-  }
-  if (select_value === 'exp') {
-    sign = '-';
-    print_to = document.querySelector('.expenses__list');
-  }
-
-  const print_list = `
-  <div class="item clearfix" id="expense-0">
-  <div class="item__description">${description_value}</div>
-  <div class="right clearfix">
-  <div class="item__value">${sign} ${inc_exp_value}</div>
-  <div class="item__percentage">21%</div>
-  <div class="item__delete">
-  <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
-  </div>
-  </div>
-  </div>
-  `;
-  const new_list = document.createElement('div');
-  new_list.innerHTML = print_list;
-  print_to.appendChild(new_list);
-
-  display_upperScreen(inc_exp_value, select_value, sign);
-  total_inc(select_value, Inc_exp_value);
 }
 
 function display_upperScreen(latest_value, select, sign) {
@@ -78,16 +110,33 @@ function formatNumber(num) {
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 }
 
-function total_inc(select, new_entry) {
+function total_inc(select, inv_expense) {
   const display_total = document.querySelector('.budget__value');
-  let current_value;
-  if (select === 'inc') {
-    current_value = current_value + new_entry;
-  }
+  let inc_sum = 0,
+    exp_sum = 0;
+  inc_array.forEach(function (obj) {
+    inc_sum = inc_sum + parseFloat(obj.inc_exp);
+    console.log(obj.inc_exp);
+  });
+  exp_array.forEach(function (obj) {
+    exp_sum += parseFloat(obj.inc_exp);
+  });
+  let total_Income = inc_sum - exp_sum;
+  display_total.innerHTML = total_Income;
+  console.log(select);
   if (select === 'exp') {
-    current_value = current_value - new_entry;
+    exp_percentage(inc_sum, inv_expense);
+  } else {
+    return;
   }
-  display_total.innerHTML = current_value;
-  // formatNumber(current_value);
-  console.log('daniel');
+}
+
+function exp_percentage(totalIncome, inv_expense) {
+  inv_percentage = Math.floor(
+    (parseFloat(inv_expense) / parseFloat(totalIncome)) * 100
+  );
+  console.log('Asuncion');
+  console.log(inv_percentage);
+  percentage_dis.innerHTML = `${inv_percentage} %`;
+  return inv_percentage;
 }
